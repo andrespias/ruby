0bfbb17ba403059314ad13b748cc2940eb5f4921
Andres Pias <andrespias@gmail.com>
2011-11-19 10:06:06 -0300

    Ruby arriba

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '0b0fe26ed1f38ec7e43380bb989d3bf6a35e90331d3f851b9ee6b64dfe4b8e96b528de56dcd5c43fdd3bc02f95f3f5d4a14da97c73a71d850d667f68dcf7f164'
